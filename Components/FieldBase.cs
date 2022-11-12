671fa4f3f020597e81e5d27f4c55cfa7411b59c7
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2022-11-12 13:42:33 +0100

    Add switch field Introduce base class for fields Replace switches with component Simplify lambda expressions

---
+++ Components/FieldBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace IdentityServerConfig.Components
+{
+    public abstract class FieldBase<TEntity, TProp> : ComponentBase
+    {
+        private Expression<Func<TEntity, TProp>>? _lastAssignedProperty;
+        private string? _propertyName;
+        private TProp? _propertyValue;
+
+        [Parameter, EditorRequired]
+        public TEntity Entity { get; set; } = default!;
+
+        [Parameter, EditorRequired] public Expression<Func<TEntity, TProp>> Property { get; set; } = default!;
+
+        [Parameter, EditorRequired]
+        public string DisplayName { get; set; } = default!;
+
+        protected string? PropertyName { get => _propertyName; }
+
+        protected TProp? PropertyValue { get => _propertyValue; }
+
+        protected override void OnParametersSet()
+        {
+            if (_lastAssignedProperty != Property)
+            {
+                _lastAssignedProperty = Property;
+                var compiledPropertyExpression = Property.Compile();
+
+                if (Property.Body is MemberExpression memberExpression)
+                {
+                    _propertyName = memberExpression.Member.Name;
+                }
+
+                _propertyValue = compiledPropertyExpression.Invoke(Entity);
+            }
+        }
+    }
+}
