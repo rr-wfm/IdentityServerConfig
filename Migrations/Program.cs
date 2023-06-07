887dc8a5e90c1c687218698e1e01bc51d15b5714
Niek van Weeghel <Niek.vanWeeghel@rr-wfm.com>
2023-06-07 09:04:35 +0200

    Added Serilog as a logger service

--- Migrations/Program.cs
+++ Migrations/Program.cs
@@ -1,10 +1,22 @@
 ﻿using IdentityServerConfig.Migrations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Exceptions;
 
 var host = Host.CreateDefaultBuilder(args).ConfigureServices((context, services) =>
 {
     services.AddHostedService<Worker>();
+}).UseSerilog((context, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .Enrich.WithMachineName()
+        .Enrich.WithProcessId()
+        .Enrich.WithThreadId()
+        .Enrich.WithProperty("LogicalServiceName", "IdentityServerConfig.Migrations");
 }).Build();
 
 await host.RunAsync();
